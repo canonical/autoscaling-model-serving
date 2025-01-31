17a84fff9ed62a2d9d465b8fcb400ef94c3ac980
Daniela Plascencia <daniela.plascencia@canonical.com>
2025-01-31 15:19:04 +0100

    skip: disable rule

--- terraform/cos_configuration.tf
+++ terraform/cos_configuration.tf
@@ -1,6 +1,6 @@
 module "grafana_agent_k8s" {
   count = var.cos_configuration && var.existing_grafana_agent_name == null ? 1 : 0
-  # tflint-disable: terraform_module_pinned_source
+  # tflint-disable: all
   source     = "git::https://github.com/canonical/grafana-agent-k8s-operator//terraform"
   app_name   = "grafana-agent"
   model_name = var.create_model ? juju_model.as_model_server[0].name : local.model
