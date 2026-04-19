8b1adfd3f5e9ac5fc9f776219ad6d3adbd220ec4
MattiaSarti <nmattia1@gmail.com>
2026-04-20 00:00:11 +0200

    fix: avoid locals in source fields on modules

--- terraform/applications.tf
+++ terraform/applications.tf
@@ -1,48 +1,48 @@
 module "istio_ingressgateway" {
-  source     = "git::https://github.com/canonical/istio-operators//charms/istio-gateway/terraform?ref=track/${local.istio_track}"
+  source     = "git::https://github.com/canonical/istio-operators//charms/istio-gateway/terraform?ref=track/1.28"
   model_name = var.create_model ? juju_model.as_model_server[0].name : var.model
   app_name   = "istio-ingressgateway"
   config     = {
     kind = "ingress",
   }
   revision   = var.istio_ingressgateway_revision
-  channel    = "${local.istio_track}/${var.risk}"
+  channel    = "1.28/${var.risk}"
 }
 
 module "istio_pilot" {
-  source     = "git::https://github.com/canonical/istio-operators//charms/istio-pilot/terraform?ref=track/${local.istio_track}"
+  source     = "git::https://github.com/canonical/istio-operators//charms/istio-pilot/terraform?ref=track/1.28"
   model_name = var.create_model ? juju_model.as_model_server[0].name : var.model
   config     = {
     default-gateway = var.istio_default_gateway,
   }
   revision   = var.istio_pilot_revision
-  channel    = "${local.istio_track}/${var.risk}"
+  channel    = "1.28/${var.risk}"
 }
 
 module "knative_operator" {
-  source     = "git::https://github.com/canonical/knative-operators//charms/knative-operator//terraform?ref=track/${local.knative_track}"
+  source     = "git::https://github.com/canonical/knative-operators//charms/knative-operator//terraform?ref=track/1.16"
   model_name = var.create_model ? juju_model.as_model_server[0].name : var.model
   revision   = var.knative_operator_revision
-  channel    = "${local.knative_track}/${var.risk}"
+  channel    = "1.16/${var.risk}"
 }
 
 module "knative_serving" {
-  source     = "git::https://github.com/canonical/knative-operators//charms/knative-serving//terraform?ref=track/${local.knative_track}"
+  source     = "git::https://github.com/canonical/knative-operators//charms/knative-serving//terraform?ref=track/1.16"
   model_name = var.create_model ? juju_model.as_model_server[0].name : var.model
   config     = {
     "istio.gateway.namespace" = var.create_model ? juju_model.as_model_server[0].name : var.model,
     "istio.gateway.name"      = var.istio_default_gateway,
   }
   revision   = var.knative_serving_revision
-  channel    = "${local.knative_track}/${var.risk}"
+  channel    = "1.16/${var.risk}"
 }
 
 module "kserve_controller" {
-  source     = "git::https://github.com/canonical/kserve-operators//charms/kserve-controller//terraform?ref=track/${local.kserve_track}"
+  source     = "git::https://github.com/canonical/kserve-operators//charms/kserve-controller//terraform?ref=track/0.17"
   model_name = var.create_model ? juju_model.as_model_server[0].name : var.model
   config     = {
     deployment-mode = "knative",
   }
   revision   = var.kserve_controller_revision
-  channel    = "${local.kserve_track}/${var.risk}"
+  channel    = "0.17/${var.risk}"
 }
--- terraform/main.tf
+++ terraform/main.tf
@@ -2,9 +2,3 @@ resource "juju_model" "as_model_server" {
   count = var.create_model ? 1 : 0
   name  = var.model
 }
-
-locals {
-  istio_track   = "1.28"
-  knative_track = "1.16"
-  kserve_track  = "0.17"
-}
