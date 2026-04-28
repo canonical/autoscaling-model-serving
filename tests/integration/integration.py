0294750a00ea4b869a982a84b3c43b23ee2b1f48
MattiaSarti <nmattia1@gmail.com>
2026-04-28 12:49:22 +0200

    test: restore previous output comparison method

--- tests/integration/integration.py
+++ tests/integration/integration.py
@@ -146,7 +146,7 @@ def test_inference_request(
     inference_response = requests.post(endpoint_url, headers=headers, data=prediction_input)
 
     assert inference_response.status_code == 200
-    assert inference_response.json() == {"predictions": [1, 1]}
+    assert inference_response.text == '{"predictions":[1,1]}'
 
 
 @tenacity.retry(
