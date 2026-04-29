52860bf3893338fe853ae7a30bda2abaa4866547
MattiaSarti <nmattia1@gmail.com>
2026-04-29 11:30:05 +0200

    test: restore original istio-ingressgateway channel

--- terraform/applications.tf
+++ terraform/applications.tf
@@ -6,7 +6,7 @@ module "istio_ingressgateway" {
     kind = "ingress",
   }
   revision = var.istio_ingressgateway_revision
-  channel  = "latest/edge/pr-691"
+  channel  = "1.28/${var.risk}"
 }
 
 module "istio_pilot" {
